5412ca7c0c06a420c0475214db2ca4c6ed363954
daliayan <daliayan5@gmail.com>
2021-07-29 17:24:54 -0400

    functions / inner functions / outer functions / objs / arguments / string interpolation

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity="go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+
+let wrapAdjective = function(added="*") {
+    return function(param="special"){
+        return `You are ${added}${param}${added}!`
+    }
+}
+
+const Calculator = {
+    add: function(a, b){
+        return a + b
+    },
+    subtract: function(a, b){
+        return a -b
+    },
+    multiply: function(a,b){
+        return a * b
+    },
+    divide: function(a,b){
+        return a / b
+    }
+}
+
+function actionApplyer(start, arrOfFunc){
+    let a = start
+    for (let i = 0; i < arrOfFunc.length; i++){
+        a = arrOfFunc[i](a)
+    }
+    return a 
+}
